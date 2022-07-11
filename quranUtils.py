cfddebcc9324b9985c7a50c492163270af6b23f7
Omar Amer <62311767+OmarAmer01@users.noreply.github.com>
2022-07-11 13:50:52 +0200

    commented out the scrapper function

--- quranUtils.py
+++ quranUtils.py
@@ -1,25 +1,25 @@
 import quranAPIwrapper as q
 import random as rand
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import requests as r
-import pandas as pd
+#import pandas as pd
 import linecache
 
-def getNumAyaCSV() -> int:
-    # Scrapes the number of ayats in all surahs from wikipedia
+# def getNumAyaCSV() -> int:
+#     # Scrapes the number of ayats in all surahs from wikipedia
 
-    url = "https://en.wikipedia.org/wiki/List_of_chapters_in_the_Quran"
-    webpage = r.request("GET", url=url)
-    soup = BeautifulSoup(webpage.content, "html.parser")
-    table = soup.find('table', 'wikitable sortable')
-    df = pd.read_html(str(table))
-    df = pd.concat(df)
-    df = df["Number of verses (Number of Rukūʿs)"]
+#     url = "https://en.wikipedia.org/wiki/List_of_chapters_in_the_Quran"
+#     webpage = r.request("GET", url=url)
+#     soup = BeautifulSoup(webpage.content, "html.parser")
+#     table = soup.find('table', 'wikitable sortable')
+#     df = pd.read_html(str(table))
+#     df = pd.concat(df)
+#     df = df["Number of verses (Number of Rukūʿs)"]
 
-    for idx in range(0,114):
-        df[idx] = df.str.split(" ")[idx][0]
+#     for idx in range(0,114):
+#         df[idx] = df.str.split(" ")[idx][0]
 
-    df.to_csv("ayat.col",header=False, index=False)
+#     df.to_csv("ayat.col",header=False, index=False)
 
     
 def getWird(wirdLen = 100):
